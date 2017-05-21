9b99db950d09168c6e5ebd9cd4e298d51ad2e712
STESPC\Stephen <steve2804@hotmail.co.uk>
2017-05-22 00:22:31 +0100

    mvc

---
+++ application/views/news/success.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Stephen
+ * Date: 22/05/2017
+ * Time: 00:00
+ */
+echo "News added!";
---
+++ application/views/pages/about.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Stephen
+ * Date: 21/05/2017
+ * Time: 23:26
+ */
+echo 'About Page';
